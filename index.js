232da18282d4b1406fafd36ffb5107245e2bdc1c
Amimu10 <amimul211@gmail.com>
2023-11-05 13:48:37 +0600

    added a bookCollection & send data to the server & get them with creating a route

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId, } = require("mongodb");
+const { MongoClient, ServerApiVersion} = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}>@cluster0.8cqq7bd.mongodb.net/?retryWrites=true&w=majority`; 
+// const uri = `mongodb+srv://reader-heaven:<password>@cluster0.8cqq7bd.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8cqq7bd.mongodb.net/?retryWrites=true&w=majority`; 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,7 +25,30 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+ const bookCollection = client.db("reader-heaven").collection("books");  
+ 
+ app.post("/books", async (req, res) => {
+    try {
+      const book = req.body;
+      const result = await bookCollection.insertOne(book);
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+  
+  app.get("/books", async (req, res) => {
+    try {
+     const cursor = bookCollection.find(); 
+      const books = await cursor.toArray(); 
+      res.send(books);
+    } catch (error) {
+      console.error("Error fetching books:", error); 
+    }
+  });
+  
+
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -32,7 +56,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
     res.send(" readers heaven  server is running");
