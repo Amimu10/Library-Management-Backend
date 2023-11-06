da089f78a8f3431211c1ce6550961b91c10f794f
Amimu10 <amimul211@gmail.com>
2023-11-06 19:08:11 +0600

    update book details route

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion} = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId, } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); 
 
-// const uri = `mongodb+srv://reader-heaven:<password>@cluster0.8cqq7bd.mongodb.net/?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8cqq7bd.mongodb.net/?retryWrites=true&w=majority`; 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
  const bookCollection = client.db("reader-heaven").collection("books");  
  
@@ -35,25 +34,79 @@ async function run() {
     } catch (error) {
       console.log(error.message);
     }
-  });
+  }); 
   
-  app.get("/books", async (req, res) => {
-    try {
-     const cursor = bookCollection.find(); 
-      const books = await cursor.toArray(); 
-      res.send(books);
-    } catch (error) {
-      console.error("Error fetching books:", error); 
+  app.get("/books/:id", async (req, res) => { 
+    try{
+        const id = req.params.id;  
+        const query = {
+          _id: new ObjectId(id)    
+    
+        };  
+        const result = await bookCollection.findOne(query); 
+        console.log(result); 
+        res.send(result);
+    }catch(error){
+        console.log(error.message); 
     }
-  });
-  
+  }); 
 
-    // await client.db("admin").command({ ping: 1 });
+  app.get("/books", async (req, res) => {
+    const cursor = bookCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);  
+  });  
+
+
+  app.get("/bookscategory/:category", async (req, res) => { 
+    const category = req.params.category; 
+   const query = {  
+       category: category 
+   }
+    console.log(category);     
+    const cursor = bookCollection.find(query);
+    const result = await cursor.toArray(); 
+    res.send(result);   
+  });
+
+  app.put("/books/:id", async (req, res) => { 
+    const id = req.params.id; 
+    const fliter = { _id: new ObjectId(id) }; 
+    const options = { upsert: true }; 
+    const updatedBook = req.body; 
+    const book = {
+      $set: {
+        image: updatedBook.image, 
+        name: updatedBook.name, 
+        quantity: updatedBook.quantity,  
+        category: updatedBook.category, 
+        author: updatedBook.author, 
+        rating: updatedBook.rating, 
+        description: updatedBook.description
+      }, 
+    };
+    const result = await bookCollection.updateOne(fliter, book, options);
+    res.send(result);
+  });
+
+  app.get("/bookDetails/:id", async (req, res) => {
+    const id = req.params.id;  
+    const query = {
+      _id: new ObjectId(id) 
+,
+    };
+    const result = await bookCollection.findOne(query); 
+    console.log(result);
+    res.send(result);
+  });
+
+
+    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
-  }
+  } 
 }
 run().catch(console.dir);
 
